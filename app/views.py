6dc501bb3d8b3fd2c6ee8439ab8693ef217f8a6d
shaikabbubakar <abbubakar950@gmail.com>
2023-01-10 10:26:16 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from app.forms import *
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.core.mail import send_mail
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 # Create your views here.
 
 def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
     return render(request,'home.html')
 
 def registration(request):
@@ -26,3 +33,20 @@ def registration(request):
             send_mail('User registration','Registration is success full','abbubakar950@gmail.com',[USO.email],fail_silently=False)
             return HttpResponse('Registration is successfull')
     return render(request,'registration.html',d)
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']        
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user and user.is_active:
+            login(request,user)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+
+    return render(request,'user_login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
